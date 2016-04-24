281f7199312ef1675f0e892d2900d6fca95244b4
Víctor Hernández <alu0100697032@ull.edu.es>
2016-04-24 11:58:40 +0100

    Class OwnGreedy added

--- src/clases/OwnGreedy.java
+++ src/clases/OwnGreedy.java
@@ -2,16 +2,32 @@ package clases;
 
 public class OwnGreedy {
 
+	/**
+	 * Constructor: OwnGreedy
+	 */
 	public OwnGreedy(){
 		
 	}
-	
+	/**
+	 * solve
+	 */
 	public Solution solve(Problem problem){
 		Solution solution = new Solution(problem.size());
+		initialSolution(problem, solution);
 		boolean stop = false;
 		while (stop != true){
 			
 		}
 		return solution;
+	}
+	/**
+	 * initialSolution
+	 */
+	public void initialSolution(Problem problem, Solution solution) {
+		for (int i = 0; i < problem.size(); i++) {
+			solution.setNodeTrue(i);
+			problem.getNodesOutsideSolution().remove((Object)i);
+		}
+		solution.setMdValue(problem.evaluate(solution));
 	}
 }
