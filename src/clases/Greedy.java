504551252e6a5e074027aece35c898a75ed5dbe1
Víctor Hernández <alu0100697032@ull.edu.es>
2016-04-24 11:57:34 +0100

    Greedy implemented

--- src/clases/Greedy.java
+++ src/clases/Greedy.java
@@ -12,49 +12,47 @@ public class Greedy {
 	}
 	/**
 	 * solve
-	 * @param problem
-	 * @return
 	 */
 	public Solution solve(Problem problem){
 		Solution solution = new Solution(problem.size());
-		double currentSummation = initialSubSet(problem, solution);
-		double improvedSumation = currentSummation;
-		double improvedMD = currentSummation/(double)solution.size();
-		int nodeToPutInSolution = -1;//indica el nodo a meter en la solucion
+		Solution solutionStar;
+		initialSolution(problem, solution); // seleccionar la arista con mayor afinidad
 		do{
-			if(nodeToPutInSolution > -1){//si se encontro una solucion que mejora la actual...actualizar
-				solution.addNode(nodeToPutInSolution);
-				currentSummation = improvedSumation;
-				problem.getNodesOutsideSolution().remove((Object)nodeToPutInSolution);//quitar del array de nodos fuera de la solucion
-			}
-			solution.setMdValue(improvedMD);
-			//recorrer los nodos de la solucion y unirlos a los que no estï¿½n a ver si mejora
-			for (int i = 0; i <solution.size(); i++) {
-				for (int j= 0; j < problem.getNodesOutsideSolution().size(); j++) {
-					if((currentSummation + problem.getDistance(solution.getNode(i), 
-							problem.getNodesOutsideSolution().get(j)))/(double)(solution.size()+1) > improvedMD){
-						
-						improvedSumation = currentSummation + problem.getDistance(solution.getNode(i), 
-								problem.getNodesOutsideSolution().get(j));
-						improvedMD = (improvedSumation/(double)(solution.size()+1)); 
-						nodeToPutInSolution = problem.getNodesOutsideSolution().get(j);
+			solutionStar = (Solution) solution.clone(); //S* = S
+			int bestNode = -1;
+			double maxMd = 0.0;
+			//encontrar el vértice que maximiza md(S U {k})
+			for (int i = 0; i < problem.size(); i++) {
+				if(solution.getNodeActive(i)) {
+					for (int j = 0; j < problem.getNodesOutsideSolution().size(); j++) {
+						solution.setNodeTrue(problem.getNodesOutsideSolution().get(j));
+						double maximumDispersion = problem.evaluate(solution);
+						if(maximumDispersion > maxMd) {
+							maxMd = maximumDispersion;
+							bestNode = problem.getNodesOutsideSolution().get(j);
+						}
+						solution.setNodeFalse(problem.getNodesOutsideSolution().get(j));
 					}
 				}
-			}	
-		}while(solution.getMdValue() < improvedMD);//repetir hasta que no mejore
-		return solution;
+			}
+			//si mejora entonces actualizamos
+			if(maxMd > solution.getMdValue()) {
+				solution.setNodeTrue(bestNode);
+				solution.setMdValue(maxMd);
+				problem.getNodesOutsideSolution().remove((Object)bestNode);
+			}
+		}while(!solution.getSubset().equals(solutionStar.getSubset()));///repetir hasta que no mejore
+		return solutionStar;
 	}
 	/**
-	 * initialSubSet
-	 * @param problem
-	 * @return
+	 * initialSolution
 	 */
-	public double initialSubSet(Problem problem, Solution solution){
+	public void initialSolution(Problem problem, Solution solution){
 		double max = 0.0;
 		ArrayList<Integer> subSet = new ArrayList<>();
 		//inicial subset
-		for (int i = 0; i < problem.getNumberOfNodes(); i++) {
-			for (int j = 0; j < problem.getNumberOfNodes(); j++) {
+		for (int i = 0; i < problem.size(); i++) {
+			for (int j = 0; j < problem.size(); j++) {
 				if(problem.getDistance(i, j) > max){
 					subSet.clear();
 					max = problem.getDistance(i, j);
@@ -64,9 +62,9 @@ public class Greedy {
 			}
 		}
 		for (int i = 0; i < subSet.size(); i++) {
-			problem.getNodesOutsideSolution().remove(subSet.get(i));
+			problem.getNodesOutsideSolution().remove((Object)subSet.get(i));
+			solution.setNodeTrue(subSet.get(i));
 		}
-		solution.setSubSet(subSet);
-		return max;
+		solution.setMdValue(problem.evaluate(solution));
 	}
 }
